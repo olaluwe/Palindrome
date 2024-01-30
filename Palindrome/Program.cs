4f5494dc7255c8ade68b9deb30a348c4d18603a4
Abiodun Olaluwe <oolaluwe@gmail.com>
2024-01-30 04:25:39 -0600

    Add another simpler implementation

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -31,8 +31,25 @@ public class Program
             }
             Console.WriteLine(isPalindrome ? "It is a Palindrome" : "It is not Palindrome");
             Console.WriteLine($"Equal halves of the Palindrome:{halfStrings}");
-            Console.Write("Do you want to continue?: ");
-            shouldContinue = Validation.GetValidBool("enter [y/n]: ");
+            Console.Write("Do you want to continue? (y/n): ");
+            shouldContinue = Validation.GetValidBool("");
         } while (shouldContinue);
+
+        bool shouldContinue1 = true;
+
+        // Another implementation
+        do
+        {
+            Console.WriteLine("Please a word to text if it is a palindrome: ");
+            string str = $"{Console.ReadLine()}".ToLower().Trim();    // convert the message into a char array
+            char[] charMessage = str.ToCharArray();
+            // Reverse the chars
+            Array.Reverse(charMessage);
+            string new_message = new String(charMessage);
+            // print it out
+            Console.WriteLine(str.Equals(new_message) ? "It is a palindrome" : "Not a palindrome");
+            Console.Write("Do you want to continue? (Y/N): ");
+            shouldContinue1 = Validation.GetValidBool("");
+        }while(shouldContinue1);
     }
 }
